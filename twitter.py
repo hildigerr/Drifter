bef3f8521140baae971690f5db7846009b586451
mcgrewz <mcgrewz@students.wwu.edu>
2016-05-16 10:39:56 -0700

    Twitter handling library: V1.0 =)

--- twitter.py
+++ twitter.py
@@ -1,34 +1,115 @@
-import configparser
+#Standard Python libs
+import configparser, os, pickle, sys
+
+#Site-libs
 import tweepy
 
-def getAPI():
-    config = configparser.ConfigParser()
-    config.read('twitter.conf')
+class Twitter(object):
+    def __init__(self, botName):
+        self.api = self.initializeAPI()
+        self.botName = botName
+        self.rawTweets = []
+        self.cleanTweets = []
 
-    if 'twitter' in config:
-        if config.has_option('twitter', 'consumer_key'):
-            consumer_key = config['twitter']['consumer_key']
+        if os.path.exists('twitter.pickle'):
+            self.loadState()
+        else:
+            self.lastTweetId = 0
+
+    def initializeAPI(self):
+        config = configparser.ConfigParser()
+        config.read('twitter.conf')
+
+        if 'twitter' in config:
+            if config.has_option('twitter', 'consumer_key'):
+                consumer_key = config['twitter']['consumer_key']
+            else:
+                return None
+            if config.has_option('twitter', 'consumer_secret'):
+                consumer_secret = config['twitter']['consumer_secret']
+            else:
+                return None
+            if config.has_option('twitter', 'access_token'):
+                access_token = config['twitter']['access_token']
+            else:
+                return None
+            if config.has_option('twitter', 'access_token_secret'):
+                access_token_secret = config['twitter']['access_token_secret']
+            else:
+                return None
         else:
             return None
-        if config.has_option('twitter', 'consumer_secret'):
-            consumer_secret = config['twitter']['consumer_secret']
-        else:
-            return None
-        if config.has_option('twitter', 'access_token'):
-            access_token = config['twitter']['access_token']
-        else:
-            return None
-        if config.has_option('twitter', 'access_token_secret'):
-            access_token_secret = config['twitter']['access_token_secret']
-        else:
-            return None
-    else:
-        return None
 
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    return tweepy.API(auth)
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        return tweepy.API(auth)
 
-def sendTweet(msg):
-    return True
+    def loadState(self):
+        with open('twitter.pickle', 'rb') as f:
+            self.lastTweetId = pickle.load(f)
 
+    def saveState(self):
+        with open('twitter.pickle', 'wb') as f:
+            pickle.dump(self.lastTweetId, f)
+
+    def getTweets(self):
+        tweets = {}
+        newTweets = []
+
+        if self.lastTweetId:
+            self.rawTweets = self.api.mentions_timeline(count=200, since_id=self.lastTweetId)
+        else:
+            self.rawTweets = self.api.mentions_timeline(count=200)
+
+        for t in self.rawTweets:
+            if t.id > self.lastTweetId:
+                self.lastTweetId = t.id
+
+            if t.user.screen_name in tweets:
+                #Add datetime comparison here
+                if tweets[t.user.screen_name][1] < t.created_at:
+                    print('Overwriting old tweet from %s' % t.user.screen_name)
+                    tweets[t.user.screen_name] = [t.text, t.created_at, t.id]
+                else:
+                    continue
+
+            tweets[t.user.screen_name] = [t.text, t.created_at, t.id]
+
+        
+        for t in tweets:
+            newTweets.append([t, cleanTweet(tweets[t][0], self.botName), tweets[t][1], tweets[t][2]])
+
+        #Don't lose the lastTweetId!
+        self.saveState()
+
+        print('%d total new tweets from %d user(s). [%d actual tweets]' % (len(self.rawTweets), len(tweets.keys()), len(newTweets)))
+
+        self.cleanTweets = newTweets
+
+        return newTweets
+
+    def sendTweet(self, msg):
+        return True
+
+def cleanTweet(msg, botName):
+    #Remove whitespace around msg
+    msg.strip()
+    
+    #Remove the botname
+    msg.replace(botName, '')
+
+    #Remove hashtags
+    newMsg = ''
+    for i in msg.split():
+        if i[:1] == '@':
+            pass
+        elif i[:1] == '#':
+            pass
+        elif i.find('://') > -1:
+            pass
+        else:
+            newMsg = newMsg.strip() + ' ' + i
+
+    newMsg = newMsg.strip()
+
+    return newMsg
